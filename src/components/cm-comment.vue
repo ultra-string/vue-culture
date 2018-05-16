44a990e0779908c82f0afe2eda3a6e4cad188fdc
Clarence2j <18611422521@163.com>
2018-05-16 13:42:58 +0800

    feat(weibo): weibo

---
+++ src/components/cm-comment.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="comment"></div>
+</template>
+<script>
+
+export default{
+    name : 'comment',
+    data (){
+        return {
+            weibo : '',
+        }
+    },
+    methods : {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
+
+
