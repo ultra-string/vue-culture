9df12c7af0a68668954225635280597c955abe92
Clarence2j <18611422521@163.com>
2018-05-16 18:16:57 +0800

    feat(cm-comment/st-comment):

--- src/components/cm-foot-nav.vue
+++ src/components/cm-foot-nav.vue
@@ -21,7 +21,9 @@ export default {
     props : {
         listArr : {
             type : Array,
-            default : []
+            default : function(){
+                return [];
+            }
         },
     },
     mounted(){
