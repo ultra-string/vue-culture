a7aafd1f5166a02fca5e8c436519e1c5d6d217a6
Jasonwang911 <396689144@qq.com>
2018-05-16 23:26:36 +0800

    build(route): 添加联系我们和公告路由

---
+++ src/containers/AboutUs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about-us">
+        关于我们
+    </div>
+</template>
+<script>
+export default {
+    name : 'AboutUs',
+    data (){
+        return {
+        }
+    },
+    created() {
+        console.log(this.$route.meta)
+    }
+}
+</script>
+<style lang="scss" scoped>
+.about-us{
+
+}
+</style>
---
+++ src/containers/Notice.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="notice">
+        <div v-for="(value, key) in path" :key="key">
+            <div>{{value}}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name : 'Notice',
+    data (){
+        return {
+            path: this.$route.meta
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.notice{
+
+}
+</style>
