e290acbd5704d4b5edc716d3bb739d0d4b6a74a9
Jasonwang911 <396689144@qq.com>
2018-05-17 15:38:49 +0800

    feat(cm-video-player): 封装视频组件

--- src/containers/AboutUs.vue
+++ src/containers/AboutUs.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="about-us">
         关于我们
+        <cm-video-player></cm-video-player>
     </div>
 </template>
+
 <script>
+import CmVideoPlayer from '@/components/cm-video-player'
+
 export default {
     name : 'AboutUs',
     data (){
         return {
         }
     },
+    components: {
+        CmVideoPlayer
+    },
     created() {
         console.log(this.$route.meta)
     }
 }
 </script>
+
 <style lang="scss" scoped>
 .about-us{
 
