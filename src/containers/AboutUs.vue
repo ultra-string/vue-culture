4f97add75dc8d68e423481115f262745d26d17d2
Clarence2j <18611422521@163.com>
2018-05-31 21:17:40 +0800

    feat(AboutUs): 联系我们

--- src/containers/AboutUs.vue
+++ src/containers/AboutUs.vue
@@ -1,21 +1,47 @@
 <template>
     <div class="about-us">
-        关于我们
-        <cm-video-player></cm-video-player>
+        <!-- 关于我们 -->
+        <!-- <cm-video-player></cm-video-player> -->
+        <cm-bradcrumb
+            style="margin:18px 0;lineHeight: 45px;fontSize:19px"
+            :navTree="navTree"
+        ></cm-bradcrumb>
+        <h1>联系我们</h1>
+        <p>总部地址：北京市西城区新文化街56号  </p>
+        <p>电话：010-63323856</p>
     </div>
 </template>
 
 <script>
-import CmVideoPlayer from '@/components/cm-video-player'
+import CmVideoPlayer from '@/components/cm-video-player';
+import CmBradcrumb from '@/components/cm-breadcrumb';
+
 
 export default {
     name : 'AboutUs',
     data (){
         return {
+            navTree: [
+                {
+                    name: '首页',
+                    path: 'CultureNews',
+                    url: '',
+                    nowPage: false,
+                    isOuter: false
+                },
+                {
+                    name: '联系我们',
+                    path: 'CultureNews',
+                    url: '',
+                    nowPage: false,
+                    isOuter: false
+                }
+            ],
         }
     },
     components: {
-        CmVideoPlayer
+        CmVideoPlayer,
+        CmBradcrumb
     },
     created() {
         console.log(this.$route.meta)
@@ -25,6 +51,14 @@ export default {
 
 <style lang="scss" scoped>
 .about-us{
-
+    h1{
+        line-height : 100px;
+        text-align : center;
+        font-size : 36px;
+    }
+    p{
+        font-size : 16px;
+        margin-bottom : 50px;
+    }
 }
 </style>
