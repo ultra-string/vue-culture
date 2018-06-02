d5e8d9844c81755c860eb0aa66f80d90177a1dfe
Clarence2j <18611422521@163.com>
2018-06-02 20:32:59 +0800

    feat(SpecialScheme):

--- src/containers/SpecialScheme.vue
+++ src/containers/SpecialScheme.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="special-scheme">
         <!-- 图片广告 -->
-        <div class="top-new-line clearfix">
-            <h1 v-if="advOne && advOne[0]" class="hand-point" @click="toUrlFn(advOne[0].url)">{{advOne[0].name}}</h1>
+        <div class="top-new-line top-new-line-one clearfix" v-if="this.newsData[0].SpecPlanHeadOne">
+            <h1 v-if="this.newsData[0].SpecPlanHeadOne[0]" class="hand-point" @click="toUrlFn(this.newsData[0].SpecPlanHeadOne[0].url)">{{this.newsData[0].SpecPlanHeadOne[0].name}}</h1>
             <div class="flex-box top-new-box">
                 <p class="hand-point" v-if="advOne && advOne.length" v-for="(item , index) in advOne" :key="index" @click="toUrlFn(item.url)">{{item.name}}</p>
             </div>
         </div>
         <!-- 图片广告2 -->
-        <div class="top-new-line clearfix">
-            <h1 v-if="advOne && advOne[0]" class="hand-point" @click="toUrlFn(advOne[0].url)">{{advOne[0].name}}</h1>
+        <div class="top-new-line clearfix" v-if="this.newsData[1].SpecPlanHeadTwo">
+            <h1 v-if="this.newsData[1].SpecPlanHeadTwo[0]" class="hand-point" @click="toUrlFn(this.newsData[1].SpecPlanHeadTwo[0].url)">{{this.newsData[1].SpecPlanHeadTwo[0].name}}</h1>
             <div class="flex-box top-new-box">
-                <p class="hand-point" v-if="advOne && advOne.length" v-for="(item , index) in advOne" :key="index" @click="toUrlFn(item.url)">{{item.name}}</p>
+                <p class="hand-point" v-if="advTwo && advTwo.length" v-for="(item , index) in advTwo" :key="index" @click="toUrlFn(item.url)">{{item.name}}</p>
             </div>
         </div>
         <!-- swiper -->
@@ -25,10 +25,11 @@
                 <cm-title 
                 title="活动讯息"
                 ></cm-title>
-                <cm-short-content class="fr list short-content" v-if="resData['活动讯息'] && resData['活动讯息'].length" :msg="resData['活动讯息']['0']"></cm-short-content>
+                <cm-short-content class="fr list short-content" v-if="resData['1005'] && resData['1005'].length" :msg="resData['1005']['0']"></cm-short-content>
                 <cm-news-list
                 :newsList="activeMsg"
                 :hasHotPoint="true"
+                class="font-color"
                 ></cm-news-list>
             </div>
         </div>
@@ -37,34 +38,28 @@
             <cm-title 
             title="看见非遗"
             ></cm-title>
-            <div class="clearfix">
-                <div class="video-line fl">
+            <div class="clearfix flex-direction see-heritage-box">
+                <div class="video-line">
                     <!-- 左上 -->
                     <div class="clearfix">
                         <cm-thumbnail
                         class="fl"
                         :width="230"
                         :height="135"
-                        title="匠人故事"
+                        :title="resData['1007'][0].bodyTitle"
                         :isTitle="true"
+                        :src="resData['1007'][0].thumbnailLink"
                         ></cm-thumbnail>
                         <cm-short-content
                         class="short-content fr"
+                        :msg="resData['1007'][0]"
                         ></cm-short-content>
                     </div>
-                    <!-- 左下 -->
-                    <div>
-                        <cm-news-list
-                        :newsList="newsList"
-                        :hasHotPoint="true"
-                        ></cm-news-list>
-                    </div>
                 </div>
-                <div class="news-line fr">
-                    <cm-news-list
-                    :newsList="newsList"
-                    :hasHotPoint="true"
-                    ></cm-news-list>
+                <div v-if="seeHeritage && seeHeritage.length" class="cm-news-list hand-point clearfix" v-for="(val, key) in seeHeritage"
+                    :key="key" @click="toDetail(val)">
+                    <i class="fl"></i>
+                    <h3 class="fl font-color">{{val.bodyTitle}}</h3>
                 </div>
             </div>
         </div>
@@ -73,16 +68,41 @@
             <cm-title 
             title="聆听非遗"
             ></cm-title>
-            <div class="top clearfix">
-                <div class="fl">
-                    <cm-news-list
-                    :newsList="newsList"
-                    :hasHotPoint="true"
-                    ></cm-news-list>
+            <div class="top clearfix flex-direction linsten-heritage-box">
+                <div v-if="listenHeritage && listenHeritage.length" class="cm-news-list hand-point clearfix" v-for="(val, key) in listenHeritage"
+                    :key="key" @click="toDetail(val)">
+                    <i class="fl"></i>
+                    <h3 class="fl font-color">{{val.bodyTitle}}</h3>
                 </div>
-                <div  class="fr">
-                    <st-video-content></st-video-content>
-                    <st-video-content></st-video-content>
+                <div class="clearfix video-line">
+                    <cm-thumbnail
+                    class="fl"
+                    :width="230"
+                    :height="135"
+                    :title="resData['1007'][0].bodyTitle"
+                    :isTitle="true"
+                    :src="resData['1007'][0].thumbnailLink"
+                    ></cm-thumbnail>
+                    <cm-short-content
+                    style="width:230px;"
+                    class="short-content fr"
+                    :msg="resData['1007'][1]"
+                    ></cm-short-content>
+                </div>
+                <div class="clearfix video-line">
+                    <cm-thumbnail
+                    class="fl"
+                    :width="230"
+                    :height="135"
+                    :title="resData['1007'][2].bodyTitle"
+                    :isTitle="true"
+                    :src="resData['1007'][2].thumbnailLink"
+                    ></cm-thumbnail>
+                    <cm-short-content
+                    style="width:230px;"
+                    class="short-content fr"
+                    :msg="resData['1007'][3]"
+                    ></cm-short-content>
                 </div>
             </div>
         </div>
@@ -91,20 +111,41 @@
             <cm-title 
             title="触摸非遗"
             ></cm-title>
-            <div class="clearfix">
-                <div class="fl">
-                    <st-video-content></st-video-content>
-                    <cm-news-list
-                    :newsList="newsList"
-                    :hasHotPoint="true"
-                    ></cm-news-list>
+            <div class="clearfix flex-box">
+                <div class="clearfix video-line">
+                    <cm-thumbnail
+                    class="fl"
+                    :width="230"
+                    :height="135"
+                    :title="resData['1008'][2].bodyTitle"
+                    :isTitle="true"
+                    :src="resData['1008'][2].thumbnailLink"
+                    ></cm-thumbnail>
+                    <cm-short-content
+                    style="width:230px;"
+                    class="short-content fr"
+                    :msg="resData['1008'][3]"
+                    ></cm-short-content>
                 </div>
-                <div class="fr">
-                    <st-video-content></st-video-content>
-                    <cm-news-list
-                    :newsList="newsList"
-                    :hasHotPoint="true"
-                    ></cm-news-list>
+                <div class="clearfix video-line">
+                    <cm-thumbnail
+                    class="fl"
+                    :width="230"
+                    :height="135"
+                    :title="resData['1008'][2].bodyTitle"
+                    :isTitle="true"
+                    :src="resData['1008'][2].thumbnailLink"
+                    ></cm-thumbnail>
+                    <cm-short-content
+                    style="width:230px;"
+                    class="short-content fr"
+                    :msg="resData['1008'][3]"
+                    ></cm-short-content>
+                </div>
+                <div v-if="touchHeritage && touchHeritage.length" class="cm-news-list hand-point clearfix" v-for="(val, key) in touchHeritage"
+                    :key="key" @click="toDetail(val)">
+                    <i class="fl"></i>
+                    <h3 class="fl font-color">{{val.bodyTitle}}</h3>
                 </div>
             </div>
         </div>
@@ -113,19 +154,26 @@
             <cm-title 
             title="品味非遗"
             ></cm-title>
-            <div class="clearfix">
-                <div class="fl">
-                    <st-video-content></st-video-content>
-                    <cm-news-list
-                    :newsList="newsList"
-                    :hasHotPoint="true"
-                    ></cm-news-list>
+            <div class="clearfix flex-direction taste-heritage-box">
+                <div class="clearfix video-line">
+                    <cm-thumbnail
+                    class="fl"
+                    :width="230"
+                    :height="135"
+                    :title="resData['1009'][0].bodyTitle"
+                    :isTitle="true"
+                    :src="resData['1009'][0].thumbnailLink"
+                    ></cm-thumbnail>
+                    <cm-short-content
+                    style="width:230px;"
+                    class="short-content fr"
+                    :msg="resData['1009'][1]"
+                    ></cm-short-content>
                 </div>
-                <div class="fr">
-                    <cm-news-list
-                    :newsList="newsList"
-                    :hasHotPoint="true"
-                    ></cm-news-list>
+                <div v-if="tasteHeritage && tasteHeritage.length" class="cm-news-list hand-point clearfix" v-for="(val, key) in tasteHeritage"
+                    :key="key" @click="toDetail(val)">
+                    <i class="fl"></i>
+                    <h3 class="fl font-color">{{val.bodyTitle}}</h3>
                 </div>
             </div>
         </div>
@@ -181,6 +229,10 @@ export default {
             activeMsg : '',
             advOne : [],
             advTwo : [],
+            seeHeritage : [],//看见非遗
+            listenHeritage : [],//聆听非遗
+            touchHeritage : [],//触摸非遗
+            tasteHeritage : [],//品味非遗
         }
     },
     components: {
@@ -201,8 +253,9 @@ export default {
         this.$get('/SpecPlanHeadSearch ').then(res => {
             console.log(res)
             this.newsData = res.data;
-            this.advOne = this.changeArr(1,0,3,this.newsData[0]['专题策划头条一组']);
-            this.advTwo = this.changeArr(1,0,3,this.newsData[1]['专题策划头条二组']);
+            this.advOne = this.changeArr(1,0,4,this.newsData[0].SpecPlanHeadOne);
+            console.log(this.advOne)
+            this.advTwo = this.changeArr(1,0,4,this.newsData[1].SpecPlanHeadTwo);
         })
     },
     methods : {
@@ -231,9 +284,18 @@ export default {
       },
       //根据类别处理数组
       changeListByType: function () {
+          let resData = this.resData;
           //活动讯息
-          this.activeMsg = this.changeArr(1,0,3,this.resData['活动讯息']);
-          console.log(this.activeMsg)
+          this.activeMsg = this.changeArr(1,0,3,resData['1005']);
+          //看见非遗
+          this.seeHeritage = this.changeArr(2,0,10,resData['1006']);
+          //聆听非遗
+          this.listenHeritage = this.changeArr(4,0,8,resData['1007']);
+          //触摸非遗
+          this.touchHeritage = this.changeArr(4,0,6,resData['1008']);
+          //品味非遗
+          this.tasteHeritage = this.changeArr(2,0,10,resData['1009']);
+          console.log(this.tasteHeritage)
       },
     }
 }
@@ -260,8 +322,31 @@ export default {
             }
         }
     }
+    .cm-news-list{
+        width : 472px;
+        line-height : 34px;
+        height : 34px;
+        i {
+            width: 6px;
+            height: 6px;
+            border-radius: 100%;
+            background: $font-hot;
+            margin: 15px 8px 0 0; 
+        }
+        h3 {
+            vertical-align: top;
+            max-width: 80%;
+            @include nowrap-ellipsis;
+        }
+    }
     // 看见非遗
     .see-heritage{
+        width : 100%;
+        height : 250px;
+        padding-bottom : 30px;
+        .see-heritage-box{
+            height :100%;
+        }
         >div{
             margin-top: 20px;
             .video-line{
@@ -277,7 +362,18 @@ export default {
     }
     // 聆听非遗
     .linsten-heritage{
+        width : 100%;
+        height : 304px;
         margin: 45px 0 57px 0;
+        padding-bottom : 40px;
+        .linsten-heritage-box{
+            height : 100%;
+        }
+        .video-line{
+            width : 47.5%;
+            height : 136px;
+            margin-bottom : 16px;
+        }
         .top{
             margin-top: 22px;
         }
@@ -289,6 +385,7 @@ export default {
     }
     // 触摸非遗
     .touch-heritage{
+        padding-bottom : 20px;
         >div{
             margin-top: 22px;
             >div{
@@ -298,6 +395,10 @@ export default {
     }
     //品味非遗
     .taste-heritage{
+        height : 270px;
+        .taste-heritage-box{
+            height : 100%;
+        }
         padding-bottom: 48px;
         border-bottom: 3px solid $font-hot;
         >div{
@@ -336,10 +437,22 @@ export default {
         margin-right: 100px;
       }
     }
+    .top-new-line-one{
+        background : #f6f3ec;
+    }
     .flex-box {
       @include display-flex;
       @include justify-content-space;
       @include flex-wrap-wrap;
+    }
+    .font-color{
+      color : #4c6f95;
+    }
+    .flex-direction{
+      @include display-flex;
+      // @include justify-content-space;
+      @include flex-wrap-wrap;
+      @include flex-direction;
     }
 
 }
