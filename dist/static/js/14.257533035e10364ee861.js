ffaa9b9b390f5fa0f6fc5ce62040539e269c0f6b
Jasonwang911 <396689144@qq.com>
2018-07-28 22:44:37 +0800

    去掉dist

--- dist/static/js/14.257533035e10364ee861.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([14],{"+/Wm":function(e,t){},IqRf:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{},[this._v("\n    我是文旅资讯首页\n")])},staticRenderFns:[]};var a=n("VU/8")({name:"CultureNews",data:function(){return{}}},r,!1,function(e){n("+/Wm")},"data-v-aa1c61c6",null);t.default=a.exports}});
-//# sourceMappingURL=14.257533035e10364ee861.js.map
